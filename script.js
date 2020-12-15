e75f917b6c9694f2ec7b22e7d99656c151adff84
snoringarvind <snoringarvind@gmail.com>
2020-12-16 00:25:53 +0530

    responsive

--- script.js
+++ script.js
@@ -8,29 +8,35 @@ function clickHandler(e) {
     lastelement.classList.add("x");
   }
   const value = e.target.value;
-  console.log(value);
+  // console.log(value);
 
   const element = document.querySelector(`.${value}`);
-  // if (element === null) {
-  //   return;
-  // }
 
-  console.log(element);
+  if (element === null) {
+    const nullElement = document.querySelector(".null");
+    nullElement.classList.remove("x");
+    console.log("currenlt there are no options");
+    // return;
+  }
 
-  // if (element === null) {
-  //   console.log("currenlt there are no options");
-  //   return;
-  // }
+  if (element !== null) {
+    const nullElement = document.querySelector(".null");
+    nullElement.classList.add("x");
+  }
   const elementAll = document.querySelector(".all").children;
 
-  console.log(elementAll);
+  // console.log(elementAll);
   for (let i = 0; i < elementAll.length; i++) {
     if (value === "all") {
       elementAll[i].style.display = "block";
     }
     if (element === null) {
       elementAll[i].style.display = "none";
+      // console.log(element[i]);
+      // nullElement.classList.remove("x");
+      // console.log(nullElement);
     }
+
     if (value !== "all" && element !== null) {
       if (elementAll[i].classList.value !== element.classList.value) {
         elementAll[i].style.display = "none";
